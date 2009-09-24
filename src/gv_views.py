012b54b35a702f8f8616a1b6b1742e22e47748a5
epage <eopage@byu.net>
2009-09-24 00:56:46 +0000

    Another fremantle todo done
    git-svn-id: file:///svnroot/gc-dialer/trunk@468 c39d3808-3fe2-4d86-a59f-b7f623ee9f21

--- src/gv_views.py
+++ src/gv_views.py
@@ -18,7 +18,6 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-@todo Add CTRL-V support to Dialpad
 @todo Touch selector for callback number
 @todo Alternate UI for dialogs (stackables)
 """
