00d582179e2f58a7ccc7ef3e86165864eb049554
epage <eopage@byu.net>
2009-09-25 17:59:18 +0000

    Prepping work to start on the next release
    git-svn-id: file:///svnroot/gc-dialer/trunk@485 c39d3808-3fe2-4d86-a59f-b7f623ee9f21

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,8 @@ import os
 
 __pretty_app_name__ = "DialCentral"
 __app_name__ = "dialcentral"
-__version__ = "1.0.6"
-__build__ = 9
+__version__ = "1.0.7"
+__build__ = 0
 __app_magic__ = 0xdeadbeef
 _data_path_ = os.path.join(os.path.expanduser("~"), ".dialcentral")
 _user_settings_ = "%s/settings.ini" % _data_path_
