892b5d07d62f81ca61381dd51d60514c3490186d
epage <eopage@byu.net>
2010-01-03 02:06:43 +0000

    Bump to 1.0.10-3; more stuff to try and work around Fremantle theme limits
    git-svn-id: file:///svnroot/gc-dialer/trunk@606 c39d3808-3fe2-4d86-a59f-b7f623ee9f21

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,7 @@ import os
 __pretty_app_name__ = "DialCentral"
 __app_name__ = "dialcentral"
 __version__ = "1.0.10"
-__build__ = 2
+__build__ = 3
 __app_magic__ = 0xdeadbeef
 _data_path_ = os.path.join(os.path.expanduser("~"), ".dialcentral")
 _user_settings_ = "%s/settings.ini" % _data_path_
