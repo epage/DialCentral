d13d6713d80c7838e7e2f802c219c93529d4a71e
Ed Page <eopage@byu.net>
2010-07-19 18:23:11 -0500

    Cleaning things up

--- hand_tests/test_directdial.py
+++ hand_tests/test_directdial.py
@@ -16,8 +16,12 @@ def main(username, password, number):
 
 	gvoice._browser.USER_AGENT = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16"
 	page = gvoice._get_page_with_token(
-		"https://www.google.com/voice/m/x?m=call&n=18004664411&f=&v=6",
+		"https://www.google.com/voice/m/x",
 		{
+			"m": "call",
+			"n": "18004664411",
+			"f": "",
+			"v": "6",
 		},
 	)
 	print page
