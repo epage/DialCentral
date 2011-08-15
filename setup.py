06a1989bb12da9d6efa2b70c0077dbe4aad6aac6
Ed Page <eopage@byu.net>
2011-08-15 17:23:58 -0500

    Bump for 1.3.7-2

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ ICON_CATEGORY="apps"
 ICON_SIZES=[32,48]
 #[[[end]]] (checksum: 6b362845c8509854ec12f330afd9c7b7)
 
-CHANGES = """* Fixing a launch issue with previous python versions
+CHANGES = """* Switching back to PyQt4
 """.strip()
 BUGTRACKER_URL = "https://bugs.maemo.org/enter_bug.cgi?product=Dialcentral"
 
