b54bf37dec9cb3ded588db88595e3b9ba52058f5
Ed Page <eopage@byu.net>
2011-09-05 17:21:14 -0500

    Bump to 1.3.10

--- setup.py
+++ setup.py
@@ -37,7 +37,8 @@ ICON_CATEGORY="apps"
 ICON_SIZES=[32,48]
 #[[[end]]] (checksum: 6b362845c8509854ec12f330afd9c7b7)
 
-CHANGES = """* Fixing alarm support (distutils removes the executable flag from python modules)
+CHANGES = """* Fixing broadcast messages from the Message tab
+* Making it more consistent on what numbers are shown
 """.strip()
 BUGTRACKER_URL = "https://bugs.maemo.org/enter_bug.cgi?product=Dialcentral"
 
